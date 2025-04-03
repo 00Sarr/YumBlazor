b8209b30a31001f9c0e8a7ce7bea1c41b2f86c5b
Sarath Kumar R <sarath@prophoenix.com>
2025-04-03 19:26:07 +0530

    Add project files.

---
+++ Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Category Create(Category category);
+        public Category Update(Category category);
+        public bool Delete(int Id);
+        public Category Get(int Id);
+        public IEnumerable<Category> GetAll();
+
+    }
+}
